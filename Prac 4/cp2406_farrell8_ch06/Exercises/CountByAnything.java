96e95540b54acea403de6f110d9de2061b746f5c
Callum Robertson <callumr1@hotmail.com>
2017-08-25 12:05:45 +1000

    Prac 4 work completed

--- Prac 4/cp2406_farrell8_ch06/Exercises/CountByAnything.java
+++ Prac 4/cp2406_farrell8_ch06/Exercises/CountByAnything.java
@@ -1,25 +1,24 @@
 package cp2406_farrell8_ch06.Exercises;
-
-import javax.swing.*;
-
+import java.util.Scanner;
 public class CountByAnything {
 
     public static void main(String args[])
     {
-        int MIN = 3;
         int MAX = 300;
         int countBy;
-        int enterBy = 10;
-        String countByString;
+        int numberPerLine;
+        Scanner keyboard = new Scanner(System.in);
 
-        countByString = JOptionPane.showInputDialog("What would you like to count by? ");
-        countBy = Integer.parseInt(countByString);
+        System.out.print("What would you like to count by? ");
+        countBy = keyboard.nextInt();
+        numberPerLine = countBy * 10;
 
-        for (int x = 0; x <= 10; x++)
-            for (int i = MIN; i <= MAX; i = i + countBy)
-                if (x == 10)
-                    System.out.print(i + "\n");
-                else
-                    System.out.print(i + " ");
+        for (int i = countBy; i <= MAX; i = i + countBy)
+        {
+            System.out.print(i + " ");
+            if (i % numberPerLine == 0)
+                System.out.println();
+
+        }
     }
 }
